a4a3f63a8b794b5111b1100d89293d54858fadda
Filipe Filardi <filipe.jesus@usp.br>
2018-02-24 10:40:47 -0300

    Initial structuring for development with Nodejs and React

--- client/src/index.js
+++ client/src/index.js
@@ -1,40 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-	BrowserRouter as Router,
-	Route,
-	Link
-} from 'react-router-dom';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
 
-import Header from './components/header/header';
-import Footer from './components/footer/footer';
-import Converter from './components/converter/converter';
-import Downloads from './components/donwloads/donwloads';
-import Search from './components/search/search';
-
-const app = {
-	// backgroundColor: 'black'
-};
-
-const pages = {
-	// backgroundColor: 'blue',
-	// marginHorizontal: '30px',
-};
-
-ReactDOM.render(
-	<Router>
-		<div style={app}>
-			<Header />
-
-			<div style={pages}>
-				<Route exact path='/' component={Converter} />
-				<Route exact path='/downloads' component={Downloads} />
-				<Route exact path='/search' component={Search} />
-			</div>
-
-			<Footer />
-		</div>
-	</Router>,
-	document.getElementById('root')
-);
-
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
