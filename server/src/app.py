51ef2ddcef52b3ac7a01287c722d27a1a9b81132
Filipe Filardi <filipe.jesus@usp.br>
2018-03-04 19:13:10 -0300

    fix json return to not create file inside server

--- server/src/app.py
+++ server/src/app.py
@@ -9,49 +9,50 @@ from openpyxl.utils import column_index_from_string
 
 app = Flask(__name__)
 
-DIR_JSON = os.path.join("uploadr/static/results/")
-
 # READ ALL ROWS IN WORKSHEET AND TRANSFORM INTO JSON
 def all_data_to_json(worksheet, filename, sheetname):
-    # with open('{}{}_{}.json'.format(DIR_JSON,filename,sheetname), 'w') as file:
-    with open('{}{}_{}.json'.format(DIR_JSON, filename,sheetname), 'w') as file:
-        max_row = worksheet.max_row
-        max_column = worksheet.max_column
+    # with open('{}_{}.json'.format(filename,sheetname), 'w') as file:
+    max_row = worksheet.max_row
+    max_column = worksheet.max_column
 
-        cabecalho = []
-        for col in worksheet.iter_rows(min_row=1, max_col=max_column, max_row=1):
-            for cell in col:
-                cabecalho.append(cell.value)
+    cabecalho = []
+    for col in worksheet.iter_rows(min_row=1, max_col=max_column, max_row=1):
+        for cell in col:
+            cabecalho.append(cell.value)
 
-        data = []
-        for row in worksheet.iter_rows(min_row=2, max_col=max_column, max_row=max_row):
-            item = {}
-            for cell in row: 
+    data = []
+    for row in worksheet.iter_rows(min_row=2, max_col=max_column, max_row=max_row):
+        item = {}
+        for cell in row: 
+            try:
+                if("Date" in cabecalho[column_index_from_string(cell.column)-1] or "Data" in cabecalho[column_index_from_string(cell.column)-1]):
+                    item["Date"] = str(cell.value)
+                    continue
+            except:
+                x = 0
+            try:
+                item[cabecalho[column_index_from_string(cell.column)-1].encode("utf-8")] = cell.value.encode("utf-8")
+            except:
                 try:
-                    if("Date" in cabecalho[column_index_from_string(cell.column)-1] or "Data" in cabecalho[column_index_from_string(cell.column)-1]):
-                        item["Date"] = str(cell.value)
-                        continue
-                except:
-                    x = 0
-                try:
-                    item[cabecalho[column_index_from_string(cell.column)-1].encode("utf-8")] = cell.value.encode("utf-8")
+                    item[cabecalho[column_index_from_string(cell.column)-1].encode("utf-8")] = cell.value
                 except:
                     try:
-                        item[cabecalho[column_index_from_string(cell.column)-1].encode("utf-8")] = cell.value
+                        item[cabecalho[column_index_from_string(cell.column)-1]] = cell.value.encode("utf-8")
                     except:
-                        try:
-                            item[cabecalho[column_index_from_string(cell.column)-1]] = cell.value.encode("utf-8")
-                        except:
-                            item['None'] = cell.value
-            data.append(item)
-        
-        json_data = {
-            "DATA_INFOS" : data,
-            "DATA_NUMBER" : max_row - 1
-        }
+                        item['None'] = cell.value
+        data.append(item)
+    
+    json_data = {
+        "FILE_NAME" : filename + "_" + sheetname,
+        "DATA_INFOS" : data,
+        "DATA_NUMBER" : max_row - 1
+    }
 
-        json.dump(json_data, file, indent = 4, ensure_ascii = False)
-        file.close()
+
+    return json_data
+
+        # json.dump(json_data, file, indent = 4, ensure_ascii = False)
+        # file.close()
 
 def identifier(data, word):
     for line in data["DATA_INFOS"]:
@@ -67,42 +68,23 @@ def index():
 
 @app.route("/api/upload", methods=["POST"])
 def upload():
-    print "\n\n\n\n\n\n"
-    
-    # print "files", request.files, "\n\n"
-    
+    json_files = []
     for upload in request.files.getlist("file"):
-        # print upload.filename
+        # try:
+        
+        filename = upload.filename.split(".")
+        filename = filename[0]
+
         wb = load_workbook(filename=upload)#, read_only=True)
         sheets = wb.sheetnames
         for sheet in sheets:
-            print sheet
+            ws = wb[sheet]
+            json_files.append(all_data_to_json(ws, filename, sheet))
+        # except:
+        #     print "erro"
 
-    return "request"
+    return "200"
     
-    # for upload in request.files.getlist("file"):
-    #     # try:
-    #     filename = upload.filename.rsplit("/")[0]
-    #     filename =  filename.split('.')[0]
-
-    #     wb = load_workbook(filename=upload)#, read_only=True)
-    #     # filename = file.split('.')
-
-    #     sheets = wb.get_sheet_names()
-    #     for sheet in sheets:
-    #         ws = wb[sheet]
-            
-    #         all_data_to_json(ws, filename, sheet)
-
-    #     # except:
-    #         # return render_template('index.html')
-
-    # return render_template('index.html')
-    # # try:
-    # #     return render_template('index.html')
-    # # except:
-    # #     return render_template('index.html')
-
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
