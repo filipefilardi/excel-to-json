b4d40967df89dcb198419d7a5078cafac858b56d
Filipe Filardi <filipe.jesus@usp.br>
2018-03-04 21:11:01 -0300

    refactor structure and start styling other components (header, search)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
-import Header from "./components/Header/Header";
-import Dashboard from "./components/Dashboard/Dashboard";
-import Search from "./components/Search/Search";
+import Header from "./Header/Header";
+import Dashboard from "./Dashboard/Dashboard";
+import Search from "./Search/Search";
 
 class App extends Component {
 	render() {
